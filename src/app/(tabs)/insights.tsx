78017844a0aa40d7c04e1dd3792560bf424c790b
soumadip-dev <soumadipmajila@gmail.com>
2026-04-12 12:00:00 +0530

    feat: implement native tab-based navigation structure with list, planner, and insights screens

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const InsightsScreen = () => {
+  return (
+    <View>
+      <Text className=" text-cyan-800">insights</Text>
+    </View>
+  );
+};
+
+export default InsightsScreen;
---
+++ src/app/(tabs)/planner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const PlannerScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-cyan-800">planner</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PlannerScreen;
