9f0ca30d2d338c7f13ce1a1a94410804b93b8f84
soumadip-dev <soumadipmajila@gmail.com>
2026-04-18 00:58:44 +0530

    feat: implement insights dashboard with user profile, statistics, and category analytics

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,11 +1,29 @@
+import ClearCompletedButton from '@/components/insights/ClearCompletedButton';
+import InsightsCategorySection from '@/components/insights/InsightsCategorySection';
+import InsightsPrioritySection from '@/components/insights/InsightsPrioritySection';
+import InsightsStatsSection from '@/components/insights/InsightsStatsSection';
+import UserProfile from '@/components/insights/UserProfile';
+import TabScreenBackground from '@/components/TabScreenBackground';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const InsightsScreen = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text className=" text-cyan-800">insights</Text>
-    </View>
+    <ScrollView
+      className="flex-1 bg-background py-4"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 20, gap: 14, paddingTop: insets.top }}
+      contentInsetAdjustmentBehavior="automatic"
+    >
+      <TabScreenBackground />
+      <UserProfile />
+      <InsightsStatsSection />
+      <InsightsCategorySection />
+      <InsightsPrioritySection />
+      <ClearCompletedButton />
+    </ScrollView>
   );
 };
 
